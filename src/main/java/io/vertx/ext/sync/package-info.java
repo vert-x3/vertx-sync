98b3e87b4626f033b626143decc039dd0d6596bb
Julien Ponge <julien.ponge@gmail.com>
2018-11-08 10:19:50 +0100

    Fix the build

--- src/main/java/io/vertx/ext/sync/package-info.java
+++ src/main/java/io/vertx/ext/sync/package-info.java
@@ -1,6 +1,4 @@
-@Document(fileName = "index.adoc")
 @ModuleGen(name = "vertx-sync", groupPackage = "io.vertx")
 package io.vertx.ext.sync;
 
 import io.vertx.codegen.annotations.ModuleGen;
-import io.vertx.docgen.Document;
