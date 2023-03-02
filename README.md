1ccaf7276d80e31daed1e5ebda9f99a73e9fd508
Julien Viet <julien@julienviet.com>
2023-03-02 14:53:31 +0100

    Remove this project in Vert.x 5

---
+++ README.md
@@ -0,0 +1,4 @@
+Vert.x Sync
+===
+
+Component removed in Vert.x 5, in favor of [Vert.x virtual threads](https://github.com/vert-x3/vertx-virtual-threads-incubator/).
--- src/main/java/io/vertx/ext/sync/package-info.java
+++
@@ -1,5 +0,0 @@
-@ModuleGen(name = "vertx-sync", groupPackage = "io.vertx")
-@Deprecated
-package io.vertx.ext.sync;
-
-import io.vertx.codegen.annotations.ModuleGen;
